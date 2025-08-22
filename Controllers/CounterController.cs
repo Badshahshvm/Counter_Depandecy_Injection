d74e18d28a4e3ed04abf1c4a1ec0a619d701474d
Shivam Kumar <115875580+Badshahshvm@users.noreply.github.com>
2025-08-22 14:52:04 +0530

    Update CounterController.cs

--- Controllers/CounterController.cs
+++ Controllers/CounterController.cs
@@ -1,1 +1,44 @@
 //Counter
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Counter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CounterController : ControllerBase
+    {
+        private readonly ICounterService _counterService;
+
+       
+        public CounterController(ICounterService counterService)
+        {
+            _counterService = counterService;
+        }
+
+        [HttpGet("increment")]
+        public IActionResult Increment()
+        {
+            int val = _counterService.Increment() + 5;
+            return Ok(new { Value = val});
+        }
+
+        [HttpGet("decrement")]
+        public IActionResult Decrement()
+        {
+            return Ok(new { Value = _counterService.Decrement() });
+        }
+
+        [HttpGet("current")]
+        public IActionResult GetCurrent()
+        {
+            return Ok(new { Value = _counterService.GetValue() });
+        }
+
+        [HttpGet]
+        public int getById(int id)
+        {
+            return id;
+        }
+    }
+}
